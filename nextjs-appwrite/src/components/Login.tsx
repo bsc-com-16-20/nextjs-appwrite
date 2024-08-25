55a1d85dffbf5344f1cee115be0988596b0275e8
bsc-com-16-20 <bsc-com-16-20@unima.ac.mw>
2024-08-25 14:48:19 +0200

    if session available push to profile

---
+++ nextjs-appwrite/src/components/Login.tsx
@@ -0,0 +1,34 @@
+"use client";
+import appwriteService from "@/appwrite/config";
+import useAuth from "@/context/userAuth";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React,{FormEvent,useState} from "react";
+
+
+const Login = () => {
+    const router = useRouter()
+    const {setAuthStatus} = useAuth()
+    const [formData,setFormData] = useState({
+        email:"",
+        password:"",
+    })
+
+    const [error,setError] = useState()
+
+    const login = async (e:FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        try {
+        const session =   await appwriteService.login(formData)
+        if(session){
+            setAuthStatus(true)
+           router.push("/profile")
+        }
+        } catch (error:any) {
+            setError(error.message)
+        }
+    }
+
+}
+
+export default Login;
