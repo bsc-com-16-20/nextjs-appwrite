f322ca87c5aee2118d865483c8e280de2e0e6057
bsc-com-16-20 <bsc-com-16-20@unima.ac.mw>
2024-08-25 15:07:47 +0200

    implemented Avatar

--- nextjs-appwrite/src/components/Avatar.tsx
+++ nextjs-appwrite/src/components/Avatar.tsx
@@ -5,3 +5,16 @@ type Props = {
     alt?:string;
 
 }
+
+const Avatar = ({img,alt}:Props) => {
+    return (
+        <div className="rounded-full overflow-hidden w-full pt-[100%] relative">
+            <div className="absolute inset-0">
+                <img src={img} alt={alt || img} />
+            </div>
+        </div>
+    );
+
+}
+
+export default Avatar;
