8ef832dfbb91bd7862da01b5e6d34b70c3f31555
bsc-com-16-20 <bsc-com-16-20@unima.ac.mw>
2024-08-25 13:47:50 +0200

    creating a form to hold email,password and name

--- nextjs-appwrite/src/components/Signup.tsx
+++ nextjs-appwrite/src/components/Signup.tsx
@@ -1,2 +1,23 @@
 "use client";
 import appwriteService from "@/appwrite/config";
+import useAuth from "@/context/userAuth";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React,{FormEvent,useState} from "react";
+
+
+const Signup = () => {
+    const [formData,setFormData] = useState({
+        email:"",
+        password:"",
+        name: "",
+    })
+    
+  return (
+    <div>
+        <h1>Signup</h1>
+    </div>
+  )
+}
+
+export default Signup;
