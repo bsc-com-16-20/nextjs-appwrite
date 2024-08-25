4c836cab5fb71710278f3deffdc3c3ef5bc53945
bsc-com-16-20 <bsc-com-16-20@unima.ac.mw>
2024-08-25 14:07:24 +0200

    pushing the user to profile page when his/her data is available

--- nextjs-appwrite/src/components/Signup.tsx
+++ nextjs-appwrite/src/components/Signup.tsx
@@ -7,12 +7,32 @@ import React,{FormEvent,useState} from "react";
 
 
 const Signup = () => {
+
+    const router = useRouter()
+
     const [formData,setFormData] = useState({
         email:"",
         password:"",
         name: "",
     })
-    
+    const [error,setError] = useState("")
+
+    const {setAuthStatus} = useAuth()
+
+    const create = async (e:FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        try {
+         const userData = await appwriteService.createUserAccount(formData); 
+         if (userData){
+            setAuthStatus(true)
+            router.push("/profile")
+         }
+        } catch (error:any) {
+           setError( error.message)
+            
+        }
+    } 
+
   return (
     <div>
         <h1>Signup</h1>
