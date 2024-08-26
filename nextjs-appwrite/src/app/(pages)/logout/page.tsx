3710cc5e007d5cb657c1ebacff7ad72991171aaa
bsc-com-16-20 <bsc-com-16-20@unima.ac.mw>
2024-08-26 05:29:10 +0200

    implemented a LogoutPage

---
+++ nextjs-appwrite/src/app/(pages)/logout/page.tsx
@@ -0,0 +1,27 @@
+"use client"
+import appwriteService from "@/appwrite/config";
+import useAuth from "@/context/userAuth";
+import { useRouter } from "next/navigation";
+import React,{useEffect} from "react";
+
+const LogoutPage =  () => {
+    const router = useRouter();
+    const {setAuthStatus} =  useAuth();
+
+    useEffect(() => {
+        appwriteService.logout().then(
+            () => {
+                setAuthStatus(false);
+                router.replace("/");
+            }
+        )
+
+    },[])
+
+    return(
+        <></>
+    )
+
+}
+
+export default LogoutPage;
